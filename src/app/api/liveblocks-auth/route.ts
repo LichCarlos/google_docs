c183b84cfc12c29eb3cc17e91fdfb8c274a6ef3d
LichCarlos <1434419455@qq.com>
2025-01-16 20:07:02 +0800

    添加默認的LIVEBLOCKS CLIENT URL 跳過vercel的檢測

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -8,7 +8,7 @@ const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
-
+const LIVEBLOCKS_CLIENT_URL = process.env.NEXT_PUBLIC_LIVEBLOCKS_CLIENT_URL || 'https://default-liveblocks-url.com';
 export async function POST(req: Request) {
   const { sessionClaims } = await auth();
   if (!sessionClaims) {
